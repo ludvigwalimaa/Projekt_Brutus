b91d7993cef7a74c19c86cee48e243ce6a56fbb1
Gabbelago <snelpiller@gmail.com>
2014-04-28 10:49:09 +0200

    lol

--- objekt/Person.java
+++ objekt/Person.java
@@ -1,5 +1,36 @@
 package projektbrutus.objekt;
 
 public class Person {
+	
+	private String liudID;
+	private String name;
+	private String personNummer;
 
+	public Person(String liuID, String name, String personNummer) {
+		
+	}
+
+	public String getLiudID() {
+		return liudID;
+	}
+
+	public void setLiudID(String liudID) {
+		this.liudID = liudID;s
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPersonNummer() {
+		return personNummer;
+	}
+
+	public void setPersonNummer(String personNummer) {
+		this.personNummer = personNummer;
+	}
 }
